f43dd3091750959c2c4fc0c5c37cb66ef11224b0
Usama <usama_1s@hotmail.com>
2025-11-08 21:13:14 +0300

    menu route

---
+++ src/pages/MenuPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MenuPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="text-white text-xl">This is a test menu page</p>
+    </div>
+  )
+}
+
+export default MenuPage
+
