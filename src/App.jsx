f7e3303ca39cf4c3a798a2c48b3ce9ab4273f10b
Usama <usama_1s@hotmail.com>
2025-11-08 22:00:58 +0300

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import MenuPage from './pages/MenuPage'
 
+function AppContent() {
+  const location = useLocation()
+  const showHeader = location.pathname === '/'
+
+  return (
+    <div className="min-h-screen bg-primary-900">
+      {showHeader && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
+      </Routes>
+      {showHeader && <Footer />}
+    </div>
+  )
+}
+
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-primary-900">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/menu" element={<MenuPage />} />
-        </Routes>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   )
 }
